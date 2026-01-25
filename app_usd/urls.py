76e3464634c7546598d057207e05a842a8e327ef
GreenYA992 <Greenya992@gmail.com>
2026-01-25 20:19:38 +0300

    'rework'

--- app_usd/urls.py
+++ app_usd/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("get-current-usd/", views.get_usd_rate, name="get_usd"),
     path("currency/<str:currency_code>/", views.get_currency_rate, name="get_currency"),
-    path("currencies", views.get_available_currencies, name="available_currencies"),
+    path("currencies/", views.get_available_currencies, name="available_currencies"),
 ]
