c138b943e2c3daaf134b7e9a80562c4a1c749463
GreenYA992 <Greenya992@gmail.com>
2026-01-29 12:54:46 +0300

    'add_conf'

---
+++ app_currency/config.py
@@ -0,0 +1,36 @@
+# Поддерживаемые валюты
+SUPPORTED_CURRENCIES = ["USD", "EUR"]
+
+# Ссылки на источник данных
+API_URLS = {
+    "CBR": "https://www.cbr-xml-daily.ru/daily_json.js"
+}
+
+# Настройки API
+API_SETTINGS = {
+    "TIMEOUT": 5, # Таймаут запроса (сек)
+}
+
+# Настройки кэширования
+CACHE_SETTINGS = {
+    "DEFAULT_COOLDOWN": 10, # Время между запросами (сек)
+    "KEY_PREFIX": "exchange_last_request_"
+}
+
+# Настройки базы данных
+DB_SETTINGS = {
+    "DEFAULT_RATE_LIMIT": 10, # Количество записей в истории
+}
+
+# Настройки ответов
+RESPONSE_SETTINGS = {
+    "JSON_INDENT": 2,
+    "ENSURE_ASCII": False,
+}
+
+# Формат времени
+TIME_FORMATS = {
+    "DISPLAY": "%d.%m.%Y %H:%M:%S"
+}
+
+
